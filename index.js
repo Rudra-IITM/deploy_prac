f8fb3dcb214e77ba76567041e7bc2db78553de34
Rudra-IITM <singhrudra5556@gmail.com>
2024-02-06 00:31:49 +0530

    Init

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT | 4000;
+
+app.get("/", (req, res) => {
+    res.json({
+        msg: "Hello World",
+    })
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at port ${PORT}`)
+})
